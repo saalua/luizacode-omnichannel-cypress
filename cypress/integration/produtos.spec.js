0a11910a61f642e2719fdbad98ee0321210cc35e
Luana Santos <saluana@outlook.com>
2021-09-10 21:37:33 -0300

    add testes da rota GET produtos/:id

--- cypress/integration/produtos.spec.js
+++ cypress/integration/produtos.spec.js
@@ -3,12 +3,27 @@
 const url = Cypress.config("baseUrl");
 
 describe('Endpoints de produtos', () => {
-  context('GET /produtos', () => {
-    it.only('deve retornar todos os produtos', () => {
-        cy.request('GET', `${url}/produtos`)
+  
+    context('GET /produtos', () => {
+    it('deve retornar todos os produtos', () => {
+        cy.request('GET', `${url}/produtos/2`)
             .should((response) => {
                 cy.log(JSON.stringify(response.body));
                 expect(response.status).to.eq(200);
+            });
+    });
+  });
+
+
+  context('GET /produtos/:id', () => {
+    it.only('deve retornar um produto por id', () => {
+        cy.request('GET', `${url}/produtos/1`)
+            .should((response) => {
+                cy.log(JSON.stringify(response.body));
+                expect(response.status).to.eq(200);
+                expect(response.body).to.have.all.keys(
+                    'id', 'produto', 'categoria', 'valor', 'descricao', 'marca'
+                  );
             });
     });
   });
