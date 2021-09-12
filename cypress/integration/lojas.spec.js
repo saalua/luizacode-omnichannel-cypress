3693045a64d07142eb729f66d4c06b242a4e6172
Luana Santos <saluana@outlook.com>
2021-09-12 18:01:27 -0300

    atualizando teste da requisicao de lojas

--- cypress/integration/lojas.spec.js
+++ cypress/integration/lojas.spec.js
@@ -7,8 +7,10 @@ context('GET /lojas', () => {
         it('deve retornar todas as lojas', () => {
             cy.request('GET', `${url}/lojas`)
                 .should((response) => {
-            cy.log(JSON.stringify(response.body));
-            expect(response.status).to.eq(200);
+            cy.log(JSON.stringify(response.body))
+            expect(response.status).to.eq(200)
+            expect(response.statusText).to.eq('OK')
+            expect(response.body).to.be.not.null
             });
         });
     });
